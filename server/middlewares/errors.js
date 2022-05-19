a74c9e77fff3fea10e7c4c73862a9c8a6d68d365
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-05-19 20:40:12 +0200

    Added errorMesage and errorStatus on errors middleware

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -2,14 +2,19 @@ const chalk = require("chalk");
 const debug = require("debug")("series:errors");
 
 const notFoundError = (req, res) => {
-  res.status(404).json({ msg: "Endpoint not found" });
   debug(chalk.red("Endpoint not found"));
+
+  res.status(404).json({ msg: "Endpoint not found" });
 };
 
 // eslint-disable-next-line no-unused-vars
 const errorDefault = (error, req, res, next) => {
-  debug(chalk.red("Error 500"));
-  res.status(500).json({ msg: "Error 500" });
+  const errorMessage = error.message ?? "General error";
+  const errorStatus = error.statusCode ?? 500;
+
+  debug(chalk.red(error.message));
+
+  res.status(errorStatus).json({ msg: errorMessage });
 };
 
 module.exports = { notFoundError, errorDefault };
